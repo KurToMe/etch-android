1d2e8de41b036cca52991ef57a06538e493a2ae1
kmelby <kurtome@gmail.com>
2014-07-19 16:27:10 -0400

    adding a b/w tile provider and simplifying map

--- app/src/main/java/kurtome/etch/app/openstreetmap/MapFragment.java
+++ app/src/main/java/kurtome/etch/app/openstreetmap/MapFragment.java
@@ -12,11 +12,16 @@ import android.view.*;
 import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
 import kurtome.etch.app.ObjectGraphUtils;
+import kurtome.etch.app.R;
 import kurtome.etch.app.location.LocationUpdatedEvent;
 import org.metalev.multitouch.controller.MultiTouchController;
 import org.osmdroid.ResourceProxy;
+import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.tilesource.ITileSource;
+import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.tileprovider.tilesource.XYTileSource;
+import org.osmdroid.util.BoundingBoxE6;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.util.ResourceProxyImpl;
 import org.osmdroid.views.MapView;
@@ -26,6 +31,7 @@ import org.osmdroid.views.overlay.compass.CompassOverlay;
 import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+import org.osmdroid.views.overlay.PathOverlay;
 
 import javax.inject.Inject;
 
@@ -48,9 +54,10 @@ public class MapFragment extends Fragment {
 
     private SharedPreferences mPrefs;
     private MapView mMapView;
+    private IMapController mMapController;
 //    private MyLocationNewOverlay mLocationOverlay;
-    private CompassOverlay mCompassOverlay;
-    private MinimapOverlay mMinimapOverlay;
+//    private CompassOverlay mCompassOverlay;
+//    private MinimapOverlay mMinimapOverlay;
     private ScaleBarOverlay mScaleBarOverlay;
     //    private RotationGestureOverlay mRotationGestureOverlay;
     private ResourceProxy mResourceProxy;
@@ -75,6 +82,7 @@ public class MapFragment extends Fragment {
 
         mResourceProxy = new ResourceProxyImpl(inflater.getContext().getApplicationContext());
         mMapView = new MapView(inflater.getContext(), 256, mResourceProxy);
+        mMapController = mMapView.getController();
         // Call this method to turn off hardware acceleration at the View level.
         // setHardwareAccelerationOff();
 
@@ -98,20 +106,21 @@ public class MapFragment extends Fragment {
 
         mPrefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
 
-        this.mCompassOverlay = new CompassOverlay(
-                context,
-                new InternalCompassOrientationProvider(context),
-                mMapView
-        );
+//        this.mCompassOverlay = new CompassOverlay(
+//                context,
+//                new InternalCompassOrientationProvider(context),
+//                mMapView
+//        );
 //        this.mLocationOverlay = new MyLocationNewOverlay(
 //                context,
 //                new GpsMyLocationProvider(context),
 //                mMapView
 //        );
 
-        mMinimapOverlay = new MinimapOverlay(context, mMapView.getTileRequestCompleteHandler());
-        mMinimapOverlay.setWidth(dm.widthPixels / 5);
-        mMinimapOverlay.setHeight(dm.heightPixels / 5);
+//        mMinimapOverlay = new MinimapOverlay(context, mMapView.getTileRequestCompleteHandler());
+//        mMinimapOverlay.setWidth(dm.widthPixels / 5);
+//        mMinimapOverlay.setHeight(dm.heightPixels / 5);
+//        mMinimapOverlay.setZoomDifference(5);
 
         mScaleBarOverlay = new ScaleBarOverlay(context);
         mScaleBarOverlay.setCentred(true);
@@ -120,21 +129,26 @@ public class MapFragment extends Fragment {
 //        mRotationGestureOverlay = new RotationGestureOverlay(context, mMapView);
 //		mRotationGestureOverlay.setEnabled(false);
 
-        mMapView.setBuiltInZoomControls(true);
-        mMapView.setMultiTouchControls(true);
+        mMapView.setBuiltInZoomControls(false);
+        mMapView.setMultiTouchControls(false);
 //        mMapView.getOverlays().add(this.mLocationOverlay);
-        mMapView.getOverlays().add(this.mCompassOverlay);
-        mMapView.getOverlays().add(this.mMinimapOverlay);
+//        mMapView.getOverlays().add(this.mCompassOverlay);
+//        mMapView.getOverlays().add(this.mMinimapOverlay);
         mMapView.getOverlays().add(this.mScaleBarOverlay);
 //        mMapView.getOverlays().add(this.mRotationGestureOverlay);
 
-        mMapView.getController().setZoom(mPrefs.getInt(PREFS_ZOOM_LEVEL, 1));
         mMapView.scrollTo(mPrefs.getInt(PREFS_SCROLL_X, 0), mPrefs.getInt(PREFS_SCROLL_Y, 0));
 
 //        mLocationOverlay.enableMyLocation();
-        mCompassOverlay.enableCompass();
+//        mCompassOverlay.enableCompass();
 
-        setHasOptionsMenu(true);
+        mMapView.setEnabled(false);
+
+        setMaxZoom();
+
+        setHasOptionsMenu(false);
+
+//        mMapView.onTouchEvent()
     }
 
     @Override
@@ -145,11 +159,11 @@ public class MapFragment extends Fragment {
         edit.putInt(PREFS_SCROLL_Y, mMapView.getScrollY());
         edit.putInt(PREFS_ZOOM_LEVEL, mMapView.getZoomLevel());
 //        edit.putBoolean(PREFS_SHOW_LOCATION, mLocationOverlay.isMyLocationEnabled());
-        edit.putBoolean(PREFS_SHOW_COMPASS, mCompassOverlay.isCompassEnabled());
+//        edit.putBoolean(PREFS_SHOW_COMPASS, mCompassOverlay.isCompassEnabled());
         edit.commit();
 
 //        this.mLocationOverlay.disableMyLocation();
-        this.mCompassOverlay.disableCompass();
+//        this.mCompassOverlay.disableCompass();
 
         super.onPause();
     }
@@ -157,21 +171,37 @@ public class MapFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        final String tileSourceName = mPrefs.getString(PREFS_TILE_SOURCE,
-                TileSourceFactory.DEFAULT_TILE_SOURCE.name());
+//        final String tileSourceName = mPrefs.getString(
+//                PREFS_TILE_SOURCE,
+//                TileSourceFactory.MAPQUESTOSM.name()
+//        );
         try {
-            final ITileSource tileSource = TileSourceFactory.getTileSource(tileSourceName);
+//            final ITileSource tileSource = TileSourceFactory.getTileSource(tileSourceName);
+            final ITileSource tileSource = new XYTileSource(
+                    "maps-stamen-toner-lite",
+                    ResourceProxy.string.unknown,
+                    1,
+                    18,
+                    256,
+                    ".png",
+                    new String[] {
+                            "http://a.tile.stamen.com/toner-lite/",
+                            "http://b.tile.stamen.com/toner-lite/",
+                            "http://c.tile.stamen.com/toner-lite/",
+                            "http://d.tile.stamen.com/toner-lite/"
+                    }
+            );
             mMapView.setTileSource(tileSource);
         }
         catch (final IllegalArgumentException e) {
             mMapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
         }
-        if (mPrefs.getBoolean(PREFS_SHOW_LOCATION, false)) {
+//        if (mPrefs.getBoolean(PREFS_SHOW_LOCATION, false)) {
 //            this.mLocationOverlay.enableMyLocation();
-        }
-        if (mPrefs.getBoolean(PREFS_SHOW_COMPASS, false)) {
-            this.mCompassOverlay.enableCompass();
-        }
+//        }
+//        if (mPrefs.getBoolean(PREFS_SHOW_COMPASS, false)) {
+//            this.mCompassOverlay.enableCompass();
+//        }
     }
 
     @Override
@@ -231,18 +261,26 @@ public class MapFragment extends Fragment {
     public void updateLocation(final LocationUpdatedEvent event) {
         mLocation = event.getLocation();
 
-        setMaxZoom();
         centerOnLocation();
     }
 
     private void centerOnLocation() {
-        mMapView.getController().setCenter(new GeoPoint(mLocation.getLatitude(), mLocation.getLongitude()));
+        double latitude = mLocation.getLatitude();
+        double longitude = mLocation.getLongitude();
+        GeoPoint point = new GeoPoint(latitude, longitude);
+        mMapController.setCenter(point);
+
+        BoundingBoxE6 boundingBox = new BoundingBoxE6(latitude, longitude, latitude, longitude);
+        mMapView.setScrollableAreaLimit(boundingBox);
     }
 
     private void setMaxZoom() {
+        int zoomLevel = mMapView.getMaxZoomLevel();
         // http://wiki.openstreetmap.org/wiki/Zoom_levels
-        int zoomLevel = 19;
-        mMapView.getController().setZoom(zoomLevel);
+        mMapController.setZoom(zoomLevel);
+
+        // Make sure there is no way to zoom out
+        mMapView.setMinZoomLevel(zoomLevel);
     }
 
 
